71c6ec4faaf4b57aa2b5005664a04bfbf093cd85
jazsgur <jazmin.erasun@gmail.com>
2022-07-08 17:44:15 -0300

    - corrección controller

--- src/main/java/ar/com/codoacodo/controllers/CreateController.java
+++ src/main/java/ar/com/codoacodo/controllers/CreateController.java
@@ -1,61 +1,36 @@
 package ar.com.codoacodo.controllers;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.Statement;
 
-import ar.com.codoacodo.connection.AdministradorDeConexiones;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import ar.com.codoacodo.daos.ProductoDAO;
+
 /*HERENCIA*/
-	//Create controller es hijo de:
+//Create Controller es hijo de :
 @WebServlet("/CreateController")
-public class CreateController extends HttpServlet{
+public class CreateController extends HttpServlet {
 
 	@Override
-	protected void doPost (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// recibe los datos del front
-		
-		// en req recibe los datos que manda el form html
-		// clave=valor
-		
-		// va a pedir una conexión (AdministradorDeConexion.getConection()
-		// inset en la db >SQL: INSERT INTO...
-		String nombre = req.getParameter("nombre");		
-		String precio = req.getParameter("precio"); // convertir en float parseFloat
-		String fechaCreacion = ""; // Se lo damos nosotrxs
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		String nombre = req.getParameter("nombre");
+		String precio = req.getParameter("precio");
 		String imagen = req.getParameter("imagen");
 		String codigo = req.getParameter("codigo");
 		
-			// pedir una Conexion: AdministradorDeConexion.getConection()
-			Connection con = AdministradorDeConexiones.getConnection();
-			if(con != null) { 
-				// insert en la db > SQL: INSERT INTO....
-				String sql = "INSERT INTO PRODUCTO (nombre, precio,fecha_creacion,imagen,codigo) ";
-				sql += "VALUES('"+nombre+"',"+precio+",CURRENT_DATE(),'"+imagen+"','"+codigo+"')";
-				
-				//control de errores
-				try {
-					Statement st = con.createStatement();			
-					st.execute(sql);
-					
-					//cierre de conexion
-					con.close();
-					
-					//getServletContext().getRequestDispatcher("/api/ListadoController").forward(req, resp);
-					
-					resp.sendRedirect(req.getContextPath()+"/api/ListadoController");
-				}catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	
+		//crear ProductoDAO
+		ProductoDAO dao = new ProductoDAO();
+		
+		//ejecutar el metodo crearProducto(parametros...)
+		dao.crearProducto(nombre, Float.parseFloat(precio), imagen, codigo);
+		
+		//ctrl+shit+o
+		//ir a la siguiente pagina
+		resp.sendRedirect(req.getContextPath()+"/api/ListadoController");
 	}
-
-
-	 
+}
